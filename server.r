58b6d8792798364fe0617682513784c0caa32592
Jessica Libman <jessicalibman@Jessicas-MacBook-Pro-2.local>
2016-03-02 00:48:02 -0800

    Widget 1 works, with comments, widget 2 does not appear

--- server.r
+++ server.r
@@ -1,35 +1,45 @@
+#server.r
+
 setwd("/Users/jessicalibman/Desktop/Info 498/a8-building-applications")
 # install.packages("dplyr")
-# install.packages("plotly")
+# install.packages("plot_ly")
 # install.packages("shiny")
 library(dplyr)
 library(plotly)
 library(shiny)
 
+# Load data
 data <- iris
 
-### Use input to create a string    
-shinyServer(
-  function(input, output) {
-    output$min <- renderText("slider")
-    # You can access the value of the widget with input$slider1, e.g.
-    output$bargraph <- renderPlot({
-      x <- data %>% select(Petal.Length,Species) %>% 
-                    slice(input$min : input$max) 
-      num_of_species <- x %>% group_by(Species) %>% 
-                              summarise(num_species = n())
-      #bins <- number_of_species
-        
-      
-      
-      
-      return (barplot(num_of_species$num_species, names = num_of_species$Species, main = "Petal and Species Information", xlab = "Species", ylab = "Frequency of Species"))
-    })
-    
-    
-    output$scatterplot <- renderPlot({ input$radio })
-  }
-)
+# Code to be executed by the shiny server
+shinyServer(function(input, output) {
+  
+  # Make a bar graph that adapts to user input
+  output$bargraph <- renderPlot({
+    # Select only relevent data from the dataframe incorporating the user input 
+    # from the sliders
+    x <- data %>% select(Petal.Length, Species) %>% 
+      filter(Petal.Length > input$slider[1] & Petal.Length < input$slider[2]) 
+    # Finds number of bars to plot
+    num_of_species <- x %>% group_by(Species) %>% 
+      summarise(num_species = n())
+    # Return a bar plot that represents the number of iris' in each species that 
+    # has a petal length in the selected range.
+    return (barplot(num_of_species$num_species, names = num_of_species$Species, 
+                  main = "Petal and Species Information",
+                  xlab = "Species", ylab = "Frequency of Species"))
+  })
+  
+  output$scatterplot <- renderPlotly({ 
+    petal <- data %>% select(Petal.Length, Petal.Width)
+    sepal <- data %>% select(Sepal.Length, Sepal.Width)
+    if(input$radio == 1){
+      return(plot_ly(petal, x = Petal.Length, y = Petal.Width))
+    } else {
+      return(plot_ly(sepal, x = Sepal.Length, y = Sepal.Width))
+    }
+  })
+})
 
 
 
--- ui.r
+++ ui.r
@@ -1,3 +1,5 @@
+#ui.r
+
 library(dplyr)
 library(plotly)
 library(shiny)
@@ -7,40 +9,29 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Iris Information"), 
     
+    # 
     sidebarLayout(
-      
+      # Create a sidebarPanel of the app
       sidebarPanel(
+        # Create a slider that has 2 slide buttons to make a range so the user can 
+        # select the values they would like to see on the bar plot
         sliderInput("slider", label = h3("Petal Length Range"), min = 0, max = 7, 
                     value = c(2, 5), step = 0.1),
-        #textOutput("min", slider$min),
-        #textOutput("max", slider$max)
-        
-        
-        
-        
-        
+        # Create radio buttons so the user can select whether they would like to see
+        # a scatter plot with information about iris' sepals or petals
         radioButtons("radio", label = h3("Petal or Sepal"),
                      choices = list("Petal" = 1, "Sepal" = 2), 
                      selected = 1)
-        
-        
-        
-        
       ),
       
       # Create a mainPanel of the app
       mainPanel(
         # Plot the output with the name "histogram"
-        plotOutput("bargraph"),
-        
-        plotOutput("scatterplot")
-        
-        
-        
-        
-      )
-      #fluidRow(verbatimTextOutput("slider")
+        plotOutput('bargraph'),
+        # Plot the output with the name "scatterplot"
+        plotlyOutput('scatterplot')
       )
     )
   )
+)
 
